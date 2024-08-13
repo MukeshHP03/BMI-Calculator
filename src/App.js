f21aa88ed9de234a5fc65a591764c33f8dd74ee6
MukeshHP03 <mukeshhadlyparamesh@gmail.com>
2024-08-14 01:30:39 +0530

    1st version

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useMemo, useState } from 'react'
+import './App.css'
 
-function App() {
+const App = () => {
+  const[weight, setWeight] = useState(70);
+  const[height, setHeight] = useState(180);
+  function onHeightChange(e){
+    setHeight(e.target.value);
+  }
+  function onWeightChange(e){
+    setWeight(e.target.value);
+  }
+  const output = useMemo(() => {
+    return calculateBMI(weight, height);
+  },[weight, height]);
+
+  function calculateBMI(weight, height){
+    let h = height / 100;
+    let w = weight;
+    h *= h;
+    let bmi = w / h;
+    bmi = parseFloat(bmi).toFixed(2);
+    return bmi;
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <main>
+      <h1>BMI Calculator</h1>
+      <div className='input-section'>
+        <p className='slider-output'>Weight: <span className='weight-value'>{weight}</span> kg</p>
+        <input className='input-slider'
+         type='range'
+         step='1'
+         min='40'
+         max='200'
+         onChange={onWeightChange}
+         />
+        <p className='slider-output'>Height: <span className='weight-value'>{height}</span> cm</p>
+        <input className='input-slider'
+         type='range'
+         step='1'
+         min='100'
+         max='300'
+         onChange={onHeightChange}
+        //  min='40'
+        //  max='200'
+         />
+      </div>
+      <div className='output-section'>
+        <p>Your BMI is:</p>
+        <p className='output'>{output}</p>
+      </div>
+    </main>
+  )
 }
 
-export default App;
+export default App
